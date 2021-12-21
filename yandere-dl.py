529615d45428d502e9bc5103c6962eb473b83106
star <91392303+0xSTAR@users.noreply.github.com>
2021-12-20 19:44:38 -0500

    danbooru support

--- yandere-dl.py
+++ yandere-dl.py
@@ -1,101 +1,168 @@
 #!/usr/bin/env python3
 
-# welcome to the spaghetti central of no shame
-
 from __future__ import print_function
 import os, sys, subprocess
 
 def ins(mod):
-    subprocess.check_call([sys.executable,'python','-m','pip','install','-U',mod])
+    subprocess.check_call([sys.executable,'-m','pip','install','-U',mod])
 
 try:import requests
 except ModuleNotFoundError:ins('requests');import requests
 try:from bs4 import BeautifulSoup
 except ModuleNotFoundError:ins('beautifulsoup4');from bs4 import BeautifulSoup
 
-image_links = []
-file_name = ''
-file_no = 0 # gets converted to string
-yandere_page = ''
-pages_of = 1
-page = 1
+import datetime, time
 
-def check_page(page): 
-    #for y in data:
-    if not page.startswith('https://yande.re/'):
-        print('Improper link. Exiting . . .');sys.exit()
-    return
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# redundant now that the link builds itself
-
-import datetime
-
-def file_name_():
-    global file_name
-    # abolish the ISO 8601 format for time!! 
-    file_name = '{} '.format(str(datetime.date.today()).replace('-','.'))+str(datetime.datetime.now().hour)+'.'+str(datetime.datetime.now().minute)+'.'+str(datetime.datetime.now().second)+'.'+str(datetime.datetime.now().microsecond)
-
-def take_input():
-    global yandere_page
-    yandere_page = 'https://yande.re/post?page=1&tags='
-
-    search = input('Search tags: ')
-    rating= input('Rating (ratings: safe, explicit, questionable) (defaults to none): ')
-    order=input('Order (rank, score) (defaults to rank): ')
-    pages = input('Pages: ') # local
-    if not pages == '':
-        if int(pages) >= 1:
-            global pages_of
-            pages_of=int(pages)
+class Yandere(object):
+    def take_input(self):
+        self.yandere_page = 'https://yande.re/post?page=1&tags='
+        search = input('Search tags: ')
+        rating= input('Rating (ratings: safe, explicit, questionable) (defaults to none): ')
+        order=input('Order (rank, score) (defaults to rank): ')
+        pages = input('Pages: ') # local
+        if not pages == '' and int(pages) >= 1:
+            self.pages_of=int(pages)
         else:
-            print(f'page cannot be value: {pages}. only values of \'\'(blank string) and a num>=1 are allowed.')   
-    if not search == '':
-        yandere_page = str(yandere_page)+str(search)
-    if rating != '':
-        yandere_page = str(yandere_page)+str('+rating:'+rating)
-    if not order == '':
-        if order == 'rank' or order == 'score':
-            yandere_page = str(yandere_page)+str('+order:'+order)
-    return   
+            print(f'page cannot be value: {pages}. only values of \'\'(blank string) and a num>=1 are allowed.')
+        if not search == '':
+            self.yandere_page += str(search.replace(' ','+'))
+        if rating != '' and rating=='safe' or rating=='explicit'or rating=='questionable':
+            self.yandere_page += '+rating:'+str(rating)
+        if not order == '' and order=='rank' or order=='score':
+            self.yandere_page += '+order:'+str(order)
+        return
 
-# parse page for all good links
-def get_links(yp): # global_list = link, yp = yandere page
-    global image_links
-    r = requests.get(yp)
-    html_doc = r.text
-    soup = BeautifulSoup(html_doc,'html.parser') 
-    for link in soup.find_all('a'):
-        temp_link = link.get('href')
-        if temp_link != None and temp_link.startswith('https://files.yande.re/'):
-            image_links.append(temp_link)
-    if image_links == []:
-        print('\n\nNo results were found. Exiting . . .'); return #sys.exit()
+    def get(self):
+        r = requests.get(self.yandere_page)
+        html_doc = r.text
+        soup = BeautifulSoup(html_doc,'html.parser')
+        for link in soup.find_all('a'):
+            temp_link = link.get('href')
+            if temp_link != None and temp_link.startswith('https://files.yande.re/'):
+                self.image_links.append(temp_link)
+        if self.image_links == []:
+            print('\n\nNo results were found . . .'); return
 
-def dl_imgs():
-    global file_no
-    for l in image_links:
-        file_name_()
-        r = requests.get(l, stream=True)
-        with open('./{}/'.format(str(datetime.date.today()))+str(file_name) + l[-4:],'wb') as file_destination:
-            try:r.raise_for_status()
-            except: # HTTPError as http_error:
-                print('An error has occured: ' + repr(http_error))
-                print('Error: '+ r.status_code+' when attempting to access{}'.format(l))
-                if r.status_code != requests.codes.ok:
-                    print(f'{r.status_code} is not an OK status code')
-                sys.exit()
-            print(f'Downloading image No. {file_no}')
-            for chunk in r.iter_content(chunk_size=128):
-                file_destination.write(chunk)
-        file_no+=1
+    def dl(self):
+        print(f'\n\n-- Downloading images from page {str(self.page)} of {str(self.pages_of)} --\n')
+        for link in self.image_links:
+            file_name = '{} '.format(str(datetime.date.today()).replace('-','.'))+str(datetime.datetime.now().hour)+'.'+str(datetime.datetime.now().minute)+'.'+str(datetime.datetime.now().second)+'.'+str(datetime.datetime.now().microsecond)
+            r = requests.get(link, stream=True)
+            if link.endswith('.jpeg'): # check for jpeg
+                file_ext = link[-5:]
+            elif link[-4:].startswith('.'): # everything else . . . (png , jpg, gif even)
+                file_ext = link[-4:]
+            with open('./{}/'.format(self.folder)+str(file_name) + file_ext,'wb') as file_destination:
+                try:r.raise_for_status()
+                except: # HTTPError as http_error:
+                    print('An unexpected error has occured: ' + repr())
+                    print('Error: '+ r.status_code+' when attempting to access{}'.format(l))
+                    if r.status_code != requests.codes.ok:
+                        print(f'{r.status_code} is not an OK status code')
+                    sys.exit()
+                print(f'Downloading image No. {self.file_no}')
+                for chunk in r.iter_content(chunk_size=128):
+                        file_destination.write(chunk)
 
-def update_page():
-    global yandere_page,page,image_links
-    image_links = []
-    page+=1
-    yandere_page = yandere_page.replace('page={}'.format(str(page-1)),'page={}'.format(str(page)))
+            self.file_no+=1
 
-import time
+    def update(self):
+        self.image_links = []
+        self.page+=1
+        self.yandere_page = self.yandere_page.replace('page={}'.format(str(self.page-1)),'page={}'.format(str(self.page))) # replace old page value (one less) with new page value
+
+    def leave_mark(self):
+        with open('./{}/{}'.format(self.folder,'yandere-dl.txt'),'w') as mark:
+            mark.write('downloaded from yande.re using yandere-dl v1.1.0: https://github.com/0xSTAR/yandere-dl')
+
+    def __init__(self):
+        self.image_links = []
+        self.file_no = 0 # gets converted to string
+        #self.yandere_page = 'https://yande.re/post?page=1&tags='
+        self.pages_of = 1
+        self.page = 1
+        self.folder = 'yande.re '+str(datetime.date.today())
+
+class Danbooru(object):
+    def take_input(self):
+        tokens = 0 # max of 2 tokens when searching
+        search = input('Search: ')
+        rating = input('Rating: ')
+        order = input('Order: ')
+        pages = input('Pages: ')
+        if pages != '' and pages >1:
+            self.pages_of=int(pages)
+        elif pages<=0:
+            print(f'pages cannot be of any value <= 0. Got: {str(pages)}. Pages must be any value >= 1')
+        if not search == '':
+            tokens+=len(search.split(' '))
+            self.danbooru_page+=search.replace(' ','+')
+        if rating != '':
+            if rating=='e':rating='explicit'
+            elif rating=='q':rating='questionable'
+            elif rating=='s': rating= 'safe'
+
+            if rating == 'explicit' or rating == 'questionable' or rating == 'safe':
+                tokens+=1 # only one rating allowed, no multiple ratings like multiple search tags
+                self.danbooru_page+='+rating:'+rating
+        if not order=='' and order=='score':
+            # token optimized to not permit order: rank which is the site default.
+            tokens+=1
+            self.danbooru_page+='+order:'+order
+        print(f'Search uses {str(tokens)} tokens')
+        if tokens > 2:
+            print(f'Max token count of 2 exceeded. Used: {str(tokens)}. Read the documentation in the README file for more info on the Danbooru downloader which has specific restrictions. Exiting . . .')
+            time.sleep(3)
+            sys.exit()
+
+    def get(self):
+        r=requests.get(self.danbooru_page)
+        if not r.status_code == requests.codes.ok:
+            print(f'Bad status code: {r.status_code}. Exiting . . .');time.sleep(2);sys.exit()
+        soup = BeautifulSoup(r.text,'html.parser')
+        for link in soup.find_all('a'):
+            preview_link=link.get('href')
+            if preview_link.startswith('https://danbooru.donmai.us/posts/'):
+                preview = requests.get(preview_link)
+                soup2 = BeautifulSoup(preview.text,'html.parser')
+                for l in soup2.find_all('a'):
+                    href_tag = l.get('href')
+                    if href_tag.startswith('https://cdn.donmai.us/original/') and not href_tag[-11:]=='?download=1':
+                        self.links.append(href_tag)
+        if self.links == []:
+            print('No results found . . .');return
+
+    def dl(self):
+        print(f'\n\n Downloading page {str(self.page)} of {str(self.pages_of)}\n')
+        for link in self.links:
+            r = requests.get(link,stream=True)
+            now = datetime.datetime.now()
+            ending = link.split('.')[3] # well that's an easier way of doing it. . .
+            try:
+                file_name='{} {}.{}.{}.{}.{}'.format(str(datetime.date.today()).replace('-','.'),str(now.hour),str(now.minute),str(now.second),str(now.microsecond),ending)
+            except NameError:
+                print(f'File type currently not supported . . . got extension: .{ending}')
+            print(f'Downloading image No. {str(self.file_no)}')
+            with open('./{}/{}'.format(self.folder,file_name),'wb') as file_dest:
+                for data in r.iter_content(chunk_size=1024): # larger for danbooru cause of videos
+                    file_dest.write(data)
+            self.file_no+=1
+
+    def update(self):
+        self.links = []
+        self.page+=1;self.danbooru_page=self.danbooru_page.replace('page={}'.format(str(self.page-1)),'page={}'.format(str(self.page)))
+
+    def leave_mark(self):
+        with open('./{}/{}'.format(self.folder,'yandere-dl.txt'),'w') as mark:
+            mark.write('downloaded from Danbooru using yandere-dl v1.1.0: https://github.com/0xSTAR/yandere-dl')
+
+    def __init__(self):
+        self.page:int = 1 # page no.
+        self.pages_of:int = 1 # no. of pages
+        self.danbooru_page:str = 'https://danbooru.donmai.us/posts?page=1&tags=' # base url for search
+        self.links = []
+        self.file_no:int = 0
+        self.folder:str = 'danbooru '+str(datetime.date.today())
 
 def init():
     # ansi art? or ascii. i don't really know what to call it.
@@ -105,26 +172,63 @@ def init():
         _.-| WELCOME TO YANDERE-DL |-._
             \      by 0xSTAR      /
     <<<<.....\___________________/....>>>>
-    
-    
-    """)
-    global image_links; image_links = []
 
-def main():
-    global page
-    take_input()
-    try:os.mkdir(path=str(datetime.date.today()),mode=511,dir_fd=None);print(f'new directory {str(datetime.date.today())} created')
-    except FileExistsError:
-        print(f'Directory {str(datetime.date.today())} already exists. Continuing . . .')
-    while page <= pages_of:
-        check_page(yandere_page)
-        get_links(yandere_page)
-        dl_imgs()
-        update_page()
-    print('\n\n\n . . . Done! Thank you for using my service... !\n')
+          ..... supported sites .....
+
+             - yande.re
+             - danbooru
+
+    """)
+    #sys.stdout.write('Supported sites: yande.re, danbooru\n')
+    site_choice = input('Which site would you like the tool to download from?')
+    if site_choice != '':
+        if site_choice == 'yande.re':
+            def main():
+                site = Yandere()
+                try:os.mkdir(path=site.folder,mode=511,dir_fd=None);print(f'new directory {site.folder} created')
+                except FileExistsError:
+                    print(f'Directory {site.folder} already exists. Continuing . . .') # debug
+                site.leave_mark()
+                while site.page <= site.pages_of:
+                    if not site.folder.endswith(str(datetime.date.today())):
+                        # for if people run this thing overnight
+                        print('A new day is upon us!')
+                        site.folder = 'yande.re '+str(datetime.date.today())
+                        try:os.mkdir(path=site.folder,mode=511,dir_fd=None)
+                        except FileExistsError:
+                            pass
+                    site.get()
+                    site.dl()
+                    site.update()
+                print('\n\n\n . . . Done! Thank you for using my service... !\n')
+        elif site_choice == 'danbooru':
+            def main():
+                site = Danbooru()
+                try:os.mkdir(path=site.folder,mode=511,dir_fd=None);print(f'new directory {site.folder} created')
+                except FileExistsError:
+                    print(f'Directory {site.folder} already exists. Continuing . . .') # debug
+                site.leave_mark()
+                while site.page <= site.pages_of:
+                    if not site.folder.endswith(str(datetime.date.today())):
+                        # for if people run this thing overnight
+                        print('A new day is upon us!')
+                        site.folder = 'danbooru '+str(datetime.date.today())
+                        try:os.mkdir(path=site.folder,mode=511,dir_fd=None)
+                        except FileExistsError:
+                            pass
+                    site.get()
+                    site.dl()
+                    site.update()
+                print('\n\n\n . . . Done! Thank you for using my service... !\n')
+        else: print(f'Error: site not supported. Got: {site_choice}. Exiting . . .');return
+
+        print('\n\n')
+    else:print('Must supply a site. Exiting . . .')
+    return
 
 if __name__ == '__main__':
     init()
-    main()
+    try:main()
+    except NameError:time.sleep(2);sys.exit()
     time.sleep(5)
     sys.exit()
