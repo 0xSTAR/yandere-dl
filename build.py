f03d3903aab91c0ff04460e15045582cce326a1d
star <91392303+0xSTAR@users.noreply.github.com>
2022-01-05 22:49:23 -0500

    fix build and zerochan dates
    zerochan dates were having issues on Windows

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import PyInstaller.__main__
 
 os = platform.system() if platform.system()=='Windows' or platform.system()=='Linux' or platform.system()=='Darwin' else sys.exit('ERROR: {} operating system not supported by the build tool.')
 is_64=True if sys.maxsize > 2**32 else False
-is_cpython = True if platform.python_implementation=='CPython' else False
+is_cpython = True if platform.python_implementation()=='CPython' else False
 py_ver=platform.python_version_tuple()
 is_legal_python=True if is_cpython and int(py_ver[0])==3 and int(py_ver[1])>= 6 and int(py_ver[1]) < 11 else sys.exit('Must be using CPython version >= 3.6 and < 3.11')
 
@@ -26,12 +26,12 @@ def main():
                     TARGET,
                     '-y','--clean',
                     '-F','-c',
-                    '--icon yandere.ico',
-                    '--target-arch {}'.format(target_arch),
-                    '-n yandere-dl{}'.format(dist_type),
+                    '--icon=yandere.ico',
+                    '--target-arch={}'.format(target_arch),
+                    '-n=yandere-dl{}'.format(dist_type),
                     '--noupx',
-                    '--distpath {}'.format(os.getcwd()),
-                    '--workpath {}'.format(os.getcwd()+'build_temp')
+                    #'--distpath {}'.format(os.getcwd()),
+                    #'--workpath {}'.format(os.getcwd()+'build_temp')
                 ])
             except:
                 dist_type='_macOS_generic'
@@ -39,11 +39,11 @@ def main():
                     TARGET,
                     '-y','--clean',
                     '-F','-c',
-                    '--icon yandere.ico',
-                    '-n yandere-dl{}'.format(dist_type),
+                    '--icon=yandere.ico',
+                    '-n=yandere-dl{}'.format(dist_type),
                     '--noupx',
-                    '--distpath {}'.format(os.getcwd()),
-                    '--workpath {}'.format(os.getcwd()+'build_temp')
+                    #'--distpath {}'.format(os.getcwd()),
+                    #'--workpath {}'.format(os.getcwd()+'build_temp')
                 ])
             return str('yandere-dl{}'.format(dist_type))
         else:
@@ -53,14 +53,14 @@ def main():
                 TARGET,
                 '-y','--clean',
                 '-F','-c',
-                '--icon yandere.ico',
-                '--target-arch {}'.format(target_arch),
-                '-n yandere-dl{}'.format(dist_type),
+                '--icon=yandere.ico',
+                '--target-arch={}'.format(target_arch),
+                '-n=yandere-dl{}'.format(dist_type),
                 '--noupx',
-                '--distpath {}'.format(os.getcwd()),
-                '--workpath {}'.format(os.getcwd()+'build_temp')
+                #'--distpath {}'.format(os.getcwd()),
+                #'--workpath {}'.format(os.getcwd()+'build_temp')
             ])
-            return str('yandere-dl{}'.format(dist_type))
+            return str('./dist/yandere-dl{}'.format(dist_type))
 
     elif os=='Linux': # Linux or Windows
         arch = '_linux64' if is_64 else '_linux32'
@@ -68,13 +68,13 @@ def main():
             TARGET,
             '-y','--clean',
             '-F',
-            '--icon yandere.ico',
-            '-n yandere-dl{}'.format(arch),
+            #'--icon=yandere.ico',
+            '-n=yandere-dl{}'.format(arch),
             '--noupx',
-            '--distpath {}'.format(os.getcwd()),
-            '--workpath {}'.format(os.getcwd()+'build_temp')
+            #'--distpath {}'.format('dist'),
+            #'--workpath {}'.format('build_temp')
         ])
-        return str('yandere-dl{}'.format(arch))
+        return str('./dist/yandere-dl{}'.format(arch))
 
     elif os=='Windows':
         arch = '_win64' if is_64 else '_win32'
@@ -82,18 +82,18 @@ def main():
             TARGET,
             '-y','--clean',
             '-F','-c',
-            '--icon yandere.ico',
-            '-n yandere-dl{}'.format(arch),
+            '--icon=yandere.ico',
+            '-n=yandere-dl{}'.format(arch),
             '--noupx',
-            '--distpath {}'.format(os.getcwd()),
-            '--workpath {}'.format(os.getcwd()+'build_temp')
+            #'--distpath={}'.format(os.getcwd()),
+            #'--workpath={}'.format(os.getcwd()+'build_temp')
         ])
-        return str('yandere-dl{}.exe'.format(arch))
+        return str('./dist/yandere-dl{}.exe'.format(arch))
 
 if __name__ == '__main__':
     happy_accident = main()
-    if os=='Linux' or os=='Darwin':
-        import subprocess
-        subprocess.call(['sudo','chmod','a+rwx',happy_accident])
+    #if os=='Linux' or os=='Darwin':
+        #import subprocess
+        #subprocess.call(['sudo','chmod','a+rwx',happy_accident])
     print('\n\n\n. . . Done ! \n')
     sys.exit()
